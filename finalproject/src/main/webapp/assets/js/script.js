e5c877838bf31b7885c5a8ec765b1df6511af148
dlstn202 <dlstn202@naver.com>
2024-03-19 15:27:19 +0900

    main

--- finalproject/src/main/webapp/assets/js/script.js
+++ finalproject/src/main/webapp/assets/js/script.js
@@ -63,27 +63,27 @@
   });
 })();
 // glamping-N9 [FllTwPhQTo]
-(function() {
-  $(function() {
-    $(".glamping-N9").each(function() {
-      const $block = $(this);
-      // Swiper
-      const swiper = new Swiper(".glamping-N9 .contents-swiper", {
-        slidesPerView: 'auto',
-        spaceBetween: 0,
-        allowTouchMove: false,
-        loop: true,
-        autoplay: {
-          delay: 3000,
-        },
-        navigation: {
-          nextEl: ".glamping-N9 .swiper-button-next",
-          prevEl: ".glamping-N9 .swiper-button-prev",
-        },
-      });
-    });
-  });
-})();
+// (function() {
+//   $(function() {
+//     $(".glamping-N9").each(function() {
+//       const $block = $(this);
+//       // Swiper
+//       const swiper = new Swiper(".glamping-N9 .contents-swiper", {
+//         slidesPerView: 'auto',
+//         spaceBetween: 0,
+//         allowTouchMove: false,
+//         loop: true,
+//         autoplay: {
+//           delay: 3000,
+//         },
+//         navigation: {
+//           nextEl: ".glamping-N9 .swiper-button-next",
+//           prevEl: ".glamping-N9 .swiper-button-prev",
+//         },
+//       });
+//     });
+//   });
+// })();
 // glamping-N33 [vfLtWpiFmM]
 (function() {
   $(function() {
